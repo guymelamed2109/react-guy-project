0631b7d2a0d1823f7911282ec8524fd6630137c4
guymelamed2109 <126718744+guymelamed2109@users.noreply.github.com>
2023-04-25 17:25:14 +0300

    change url may 12

--- src/pages/Memories/Memories.jsx
+++ src/pages/Memories/Memories.jsx
@@ -14,7 +14,7 @@ export const Memories = (props) => {
 
 	const May = {month:"May 2022", days:[
 		{day: 1}, {day: 2}, {day: 3}, {day: 4}, {day: 5}, {day: 6, pic: ['../../src/assets/images/may-6.1.jpg', '../../src/assets/images/may-6.2.jpg']}, {day: 7},
-		{day: 8}, {day: 9}, {day: 10}, {day: 11}, {day: 12, pic: ['../../src/assets/images/may-12.JPEG', '../../src/assets/images/may-12.1.gif']}, {day: 13}, {day: 14},
+		{day: 8}, {day: 9}, {day: 10}, {day: 11}, {day: 12, pic: ['https://guymelamed2109.github.io/src/assets/images/may-12.JPEG', '../../src/assets/images/may-12.1.gif']}, {day: 13}, {day: 14},
 		{day: 15}, {day: 16}, {day: 17}, {day: 18}, {day: 19}, {day: 20}, {day: 21},
 		{day: 22}, {day: 23}, {day: 24}, {day: 25}, {day: 26}, {day: 27}, {day: 28},
 		{day: 29}, {day: 30}, {day: 31}, {}, {}, {}, {}]}
