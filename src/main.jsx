f33754fdede0ad373a6c7ad67b33bd6a72b34a78
guymelamed2109 <guymelamed2109@gmail.com>
2023-05-20 01:46:05 +0300

    deleted pic from march 10

--- src/main.jsx
+++ src/main.jsx
@@ -93,7 +93,7 @@ import { Photos } from './pages/Photos/Photos'
 
 	const March = {month:"March 2023", days:[
 		{}, {}, {}, {day: 1}, {day: 2}, {day: 3}, {day: 4},
-		{day: 5}, {day: 6}, {day: 7}, {day: 8}, {day: 9}, {day: 10, pic:['https://guymelamed2109.github.io/react-guy-project/images/march-10.1.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.2.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.3.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.4.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.5.JPEG']}, {day: 11, pic:['https://guymelamed2109.github.io/react-guy-project/images/march-11.JPEG']},
+		{day: 5}, {day: 6}, {day: 7}, {day: 8}, {day: 9}, {day: 10, pic:['https://guymelamed2109.github.io/react-guy-project/images/march-10.1.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.2.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.3.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-10.4.JPEG']},
 		{day: 12}, {day: 13}, {day: 14}, {day: 15}, {day: 16}, {day: 17}, {day: 18},
 		{day: 19}, {day: 20}, {day: 21}, {day: 22}, {day: 23}, {day: 24}, {day: 25},
 		{day: 26}, {day: 27}, {day: 28}, {day: 29}, {day: 30}, {day: 31, pic:['https://guymelamed2109.github.io/react-guy-project/images/march-31.1.JPEG', 'https://guymelamed2109.github.io/react-guy-project/images/march-31.2.JPEG']}, {}]}
